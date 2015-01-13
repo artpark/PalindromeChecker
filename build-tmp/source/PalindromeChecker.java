3f07347d092f7e60999e66beabba379f2f6e6d3c
unknown <thparkeenvincha@697-334-Aragorn.sf.k12.internal>
2015-01-13 10:20:02 -0800

    Palindrome Checker

--- build-tmp/source/PalindromeChecker.java
+++ build-tmp/source/PalindromeChecker.java
@@ -18,24 +18,38 @@ public void setup()
 {
   String lines[] = loadStrings("palindromes.txt");
   println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
+  for (int i = 0; i < lines.length; i++)
   {
-    if(palindrome(lines[i])==true)
+    if (isPalindrome(lines[i]) == true)
     {
-      println(lines[i] + " IS a palidrome.");
-    }
-    else
+      println(lines[i] + " IS a palindrome.");
+    } else
     {
-      println(lines[i] + " is NOT a palidrome.");
+      println(lines[i] + " is NOT a palindrome.");
     }
   }
 }
-public boolean palindrome(String word)
-{
-  //your code here
-  return false;
-}
 
+public boolean isPalindrome(String word)
+{
+  String justLetters = "";
+  word = word.toLowerCase();
+  for (int i=0; i<word.length (); i++)
+  {
+    if (word.charAt(i) >= 97 && word.charAt(i) <= 122)
+    {
+      justLetters += word.charAt(i);
+    }
+  }
+  for (int i=0; i<=word.length()/2; i++)
+  {
+    if(justLetters.charAt(i) != justLetters.charAt(justLetters.length()-1-i))
+    {
+      return false;
+    }
+  }
+  return true;
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "PalindromeChecker" };
     if (passedArgs != null) {
